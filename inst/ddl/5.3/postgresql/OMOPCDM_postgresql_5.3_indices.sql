9387056f670ac7a4b4c920b2354cbef6f9037d32
Min Kim <min.kim@cipherome.com>
2023-09-10 17:02:25 -0700

    additional indexes for the concept table

--- inst/ddl/5.3/postgresql/OMOPCDM_postgresql_5.3_indices.sql
+++ inst/ddl/5.3/postgresql/OMOPCDM_postgresql_5.3_indices.sql
@@ -142,6 +142,8 @@ CREATE INDEX idx_concept_code ON @cdmDatabaseSchema.concept (concept_code ASC);
 CREATE INDEX idx_concept_vocabluary_id ON @cdmDatabaseSchema.concept (vocabulary_id ASC);
 CREATE INDEX idx_concept_domain_id ON @cdmDatabaseSchema.concept (domain_id ASC);
 CREATE INDEX idx_concept_class_id ON @cdmDatabaseSchema.concept (concept_class_id ASC);
+CREATE INDEX idx_concept_name ON @cdmDatabaseSchema.concept (concept_name ASC);
+CREATE INDEX idx_concept_standard_concept ON @cdmDatabaseSchema.concept (standard_concept ASC);
 
 CREATE INDEX idx_vocabulary_vocabulary_id  ON @cdmDatabaseSchema.vocabulary  (vocabulary_id ASC);
 CLUSTER @cdmDatabaseSchema.vocabulary  USING idx_vocabulary_vocabulary_id ;
